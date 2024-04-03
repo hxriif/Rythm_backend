8238eefd5faf491805faad14870aed5745bb2679
hxriif <zaidbnuarifc@gmail.com>
2024-04-03 15:57:59 +0530

    admin-fetched_allusers

--- Controllers/AdminController.js
+++ Controllers/AdminController.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken")
+const usercollection = require("../Models/userSchema")
 
 
 module.exports = {
 
 
-    login: async (req, res) => {  
+    login: async (req, res) => {
         const { email, password } = req.body;
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
             const token = jwt.sign({ email }, process.env.ADMIN_ACCESS_TOKEN_SECRET);
@@ -22,6 +23,24 @@ module.exports = {
         }
     },
 
+
+    getallusers: async (req, res) => {
+        const allusers = await usercollection.find();
+        if (allusers.length === 0) {
+            return res.status(404).json({
+                status: "not found",
+                message: "user list is empty"
+            })
+        }
+        else {
+            return res.status(200).json({
+                status: "success",
+                message: "user's fetched sucessfully",
+                data: allusers
+            })
+        }
+
+    }
 
 
 
--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Admincontroller = require("../Controllers/AdminController");
 const TrycatchMiddleware = require('../Middlewares/Trycatchmiddleware');
+const verifyToken=require("../Middlewares/adminAuthMiddleware")
 
 router.post('/login', TrycatchMiddleware(Admincontroller.login));
+
+router.use(verifyToken)
+
+router.get('/allusers',TrycatchMiddleware(Admincontroller.getallusers))
+
+
 
 module.exports = router;
