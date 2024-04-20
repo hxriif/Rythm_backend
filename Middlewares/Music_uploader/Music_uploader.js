8c4e43919ab8ecb2dd46208926f17d922a58bed9
hxriif <zaidbnuarifc@gmail.com>
2024-04-20 10:13:08 +0530

    updates_in_music upload

--- Middlewares/Music_uploader/Music_uploader.js
+++ Middlewares/Music_uploader/Music_uploader.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "uploadss"), 
+    destination: path.join(__dirname, "uploads"), 
     filename: (req, file, cb) => {
         const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
         const extension = path.extname(file.originalname);
