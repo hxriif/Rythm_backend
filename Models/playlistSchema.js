bb8edb0430b5edc2bd3aec76a18c1d5893905348
hxriif <zaidbnuarifc@gmail.com>
2024-04-24 11:03:47 +0530

    playlistSchema_modified

--- Models/playlistSchema.js
+++ Models/playlistSchema.js
@@ -5,7 +5,11 @@ const playlistSchema= new mongoose.Schema({
     name:String,
     description:String,
 
-
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+     
     songs:[{
         songsId: { type: mongoose.Schema.ObjectId, ref: "song" },
     }]
