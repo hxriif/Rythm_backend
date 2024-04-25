8a663207161ac47a1df9a638c254e183ea09ce90
hxriif <zaidbnuarifc@gmail.com>
2024-04-25 10:20:05 +0530

    changes

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -2,38 +2,40 @@ const bcrypt = require("bcrypt")
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    username: String,
-    password: String,
-    image:String,
+  name: String,
+  email: String,
+  username: String,
+  password: String,
+  image: String,
 
 
-    createdPlaylists: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Playlist"
+  createdPlaylists: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Playlist"
   }],
 
-    Likedsongs: [
-        {
-          musicsId: { type: mongoose.Schema.ObjectId, ref: "musics" },
-        },
-      ],
+  Likedsongs: [
+    {
+      musicsId: { type: mongoose.Schema.ObjectId, ref: "musics" },
+    },
+  ],
 
-      createdPlaylists: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Playlist"
-    }],
+  createdPlaylists: [
+    {
+      type: mongoose.Schema.Types.ObjectId, ref: "Playlist"
+
+    }
+  ],
 });
 
 
 userSchema.pre("save", async function (next) {
-    const user = this;
-    if (!user.isModified("password")) return next();
+  const user = this;
+  if (!user.isModified("password")) return next();
 
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
-    next();
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+  next();
 });
 
 module.exports = mongoose.model('user', userSchema);
