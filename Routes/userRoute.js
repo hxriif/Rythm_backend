a1bc67953223bd998d31d0bfcf95603bc56cb443
hxriif <zaidbnuarifc@gmail.com>
2024-04-25 10:20:15 +0530

    changes

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -2,24 +2,31 @@ const express = require('express')
 const router = express.Router()
 const userController = require("../Controllers/UserController")
 const TrycatchMiddleware = require("../Middlewares/Trycatchmiddleware")
-const verifyToken=require("../Middlewares/userAuthMiddleware")
-const imageuploader=require("../Middlewares/Image_uploader/Img_uploader")
+const verifyToken = require("../Middlewares/userAuthMiddleware")
+const imageuploader = require("../Middlewares/Image_uploader/Img_uploader")
 
-
-router.post("/Register",imageuploader,TrycatchMiddleware(userController.register))
+// authentication & authorization
+router.post("/Register", imageuploader, TrycatchMiddleware(userController.register))
 router.post("/login", TrycatchMiddleware(userController.login))
 
 router.use(verifyToken)
-router.get('/getallMusics',TrycatchMiddleware(userController.getAllsongs))
-router.get('/getmusicByid/:id',TrycatchMiddleware(userController.musicById))
-router.get('/getmusicByCategory/:categoryname',TrycatchMiddleware(userController.musicByCategoryName))
-router.post('/addToLikedSongs/:id',TrycatchMiddleware(userController.addToLikedSongs))
-router.get('/viewLikedSongs/:id',TrycatchMiddleware(userController.viewLikedSongs))
-router.delete('/deleteFromLikedSongs/:id',TrycatchMiddleware(userController.deleteFromLikedSongs))
-router.post('/createPlaylist/:id',TrycatchMiddleware(userController.createPlaylist))
-router.post('/addMusicToPlaylist/:playlistId',TrycatchMiddleware(userController.addSongToPlaylist))
-router.get('/getUserPlaylists/:id',TrycatchMiddleware(userController.getUserPlaylist))
-router.get('/playlistSongs/:id',TrycatchMiddleware(userController.viewPlaylistSongs))
+// fetching music
+router.get('/getallMusics', TrycatchMiddleware(userController.getAllsongs))
+router.get('/getmusicByid/:id', TrycatchMiddleware(userController.musicById))
+router.get('/getmusicByCategory/:categoryname', TrycatchMiddleware(userController.musicByCategoryName))
+
+// liked songs setup
+router.post('/addToLikedSongs/:id', TrycatchMiddleware(userController.addToLikedSongs))
+router.get('/viewLikedSongs/:id', TrycatchMiddleware(userController.viewLikedSongs))
+router.delete('/deleteFromLikedSongs/:id', TrycatchMiddleware(userController.deleteFromLikedSongs))
+
+// playlist setup
+router.post('/createPlaylist/:id', TrycatchMiddleware(userController.createPlaylist))
+router.delete('/deletePlaylist/:id', TrycatchMiddleware(userController.deletePlaylist))
+router.get('/getUserPlaylists/:id', TrycatchMiddleware(userController.getUserPlaylist))
+router.post('/addMusicToPlaylist/:playlistId', TrycatchMiddleware(userController.addSongToPlaylist))
+router.delete('/deletePlaylisSong/:id', TrycatchMiddleware(userController.deletePlaylistSongs))
+router.get('/playlistSongs/:id', TrycatchMiddleware(userController.viewPlaylistSongs))
 
 
 
