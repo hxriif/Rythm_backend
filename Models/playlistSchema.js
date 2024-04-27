8911a9c92a176c6f31695dcbb2657f79eadbbb0f
hxriif <zaidbnuarifc@gmail.com>
2024-04-27 11:53:57 +0530

    changes

--- Models/playlistSchema.js
+++ Models/playlistSchema.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
 
-const playlistSchema= new mongoose.Schema({
-    name:String,
-    description:String,
+const playlistSchema = new mongoose.Schema({
+    name: String,
+    description: String,
 
     creator: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
-     
-    songs:[{
-          type: mongoose.Schema.Types.ObjectId, ref: "musics" ,
+
+    songs: [{
+        type: mongoose.Schema.Types.ObjectId, ref: "musics",
     }]
 })
 
 
-module.exports=mongoose.model('Playlist', playlistSchema)
+module.exports = mongoose.model('Playlist', playlistSchema)
