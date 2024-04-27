e31a3567efcc058c5b62cde09df751ed80fabaaa
hxriif <zaidbnuarifc@gmail.com>
2024-04-27 11:53:48 +0530

    changes

---
+++ Models/MusicUploadRequest.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+
+const musicUploadRequestSchema = new mongoose.Schema({
+
+    name: String,
+    image: String,
+    category: String,
+    description: String,
+    artist: String,
+    song: String,
+
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+    },
+
+
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending'
+    },
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = mongoose.model("PendingRequest", musicUploadRequestSchema)
